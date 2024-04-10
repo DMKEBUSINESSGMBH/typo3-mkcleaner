efdafde9fd3bf541c40b414a97966108ec9de488
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2024-04-10 16:41:41 +0200

    Bugfix fix tests again

--- Tests/Unit/Service/Mat2ServiceTest.php
+++ Tests/Unit/Service/Mat2ServiceTest.php
@@ -84,7 +84,7 @@ class Mat2ServiceTest extends UnitTestCase
             ->with(
                 'exec',
                 [
-                    'cmd' => dirname(__FILE__).'/../../Fixtures/mat2 --inplace --lightweight /var/www/html/packages/mkcleaner/Tests/Unit/Service/../../Fixtures/Files/dummy.txt',
+                    'cmd' => dirname(__FILE__).'/../../Fixtures/mat2 --inplace --lightweight '.realpath(dirname(__FILE__).'/../../Fixtures/Files/dummy.txt'),
                     'output' => ['mat2 executed'],
                     'returnValue' => 123,
                 ]
